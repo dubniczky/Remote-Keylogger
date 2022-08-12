cde89db25290754b1d48d0155b63c99b5a4e30cb
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-08-12 20:29:36 +0200

    Added globals to server

--- server.py
+++ server.py
@@ -1,7 +1,13 @@
+import os
 import uvicorn
 from fastapi import FastAPI
 from fastapi.requests import Request
 from pydantic import BaseModel
+
+
+host = "127.0.0.1"
+port = 9998
+logs_dir = os.path.join(os.path.dirname(__file__), 'logs')
 
 
 class LogData(BaseModel):
@@ -10,11 +16,15 @@ class LogData(BaseModel):
 
 app = FastAPI()
 
+
 @app.post("/log")
 def log_data(data: LogData, request: Request):
     host = request.client.host
     print(host, data.data)
-    with open(f'logs/{host}.log', 'a') as f:
+    with open(f'{logs_dir}/{host}.log', 'a') as f:
         f.write(data.data)
-    
-uvicorn.run(app, host="127.0.0.1", port=9998)
+
+
+if __name__ == '__main__':
+    os.mkdir(logs_dir, exist_ok=True)
+    uvicorn.run(app, host=host, port=port)
